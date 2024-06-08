6a2199bed2264f7519f58b33295027f43a83b1d5
s4n9hyun <s4n9hyun@gmail.com>
2024-06-09 02:48:02 +0900

    jieun_merge2

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ import Animation3 from "./pages/Animation3";
 import Animation4 from "./pages/Animation4";
 import NextButtonScreen from "./pages/NextButtonScreen";
 import ResultScreen from "./pages/ResultScreen";
-<<<<<<< HEAD
-=======
 import Guide from "./pages/Guide"; // 추가된 부분
->>>>>>> c740b3e (jieun_merge)
 
 const App = () => {
     const [name, setName] = useState("");
@@ -61,10 +58,7 @@ const App = () => {
                 <Route path="/animation4" element={<Animation4 />} />
                 <Route path="/nextButtonScreen" element={<NextButtonScreen setScreen={navigate} />} />
                 <Route path="/result" element={<ResultScreen resetGame={resetGame} />} />
-<<<<<<< HEAD
-=======
                 <Route path="/guide" element={<Guide />} /> {/* 추가된 부분 */}
->>>>>>> c740b3e (jieun_merge)
             </Routes>
         </div>
     );
