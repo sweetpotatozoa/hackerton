e9d2ae851e1d8900c8568a5b82ffc6550cec9344
s4n9hyun <s4n9hyun@gmail.com>
2024-06-09 07:50:14 +0900

    change.js modified & iphonenoti.m4a

--- src/pages/Change/Change.js
+++ src/pages/Change/Change.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import styles from './Change.module.css';
 import { useParams, useNavigate } from 'react-router-dom';
 
@@ -5,6 +6,15 @@ const Change = (props) => {
     const { chapter, setChapter, gameContent } = props;
     const { chapterId } = useParams();
     const navigate = useNavigate();
+
+    const [gender, setGender] = useState('');
+
+    useEffect(() => {
+        const savedGameData = JSON.parse(localStorage.getItem('gameData'));
+        if (savedGameData && savedGameData.gender) {
+            setGender(savedGameData.gender);
+        }
+    }, []);
 
     const nextHandler = () => {
         if (chapterId === '8') {
@@ -18,34 +28,22 @@ const Change = (props) => {
 
     const imgChanger = { backgroundImage: `url(${gameContent.change.url})` };
 
+    const faceImages = gender === 'male' 
+        ? ['/images/face1w.png', '/images/face2w.png', '/images/face3w.png', '/images/face4w.png']
+        : ['/images/face1.png', '/images/face2.png', '/images/face3.png', '/images/face4.png'];
+
     return (
         <div className={styles.background} onClick={nextHandler}>
             <div className={styles.blurImage} style={imgChanger}></div>
             <div className={styles.faces}>
-                <div className={styles.face}>
-                    <img src={'/images/face1.png'} alt="change" />
-                    <div className={styles.gauge}>
-                        <div className={styles.fill} style={{ width: '50%' }}></div>
+                {faceImages.map((faceImage, index) => (
+                    <div className={styles.face} key={index}>
+                        <img src={faceImage} alt="change" />
+                        <div className={styles.gauge}>
+                            <div className={styles.fill} style={{ width: '50%' }}></div>
+                        </div>
                     </div>
-                </div>
-                <div className={styles.face}>
-                    <img src={'/images/face2.png'} alt="change" />
-                    <div className={styles.gauge}>
-                        <div className={styles.fill} style={{ width: '50%' }}></div>
-                    </div>
-                </div>
-                <div className={styles.face}>
-                    <img src={'/images/face3.png'} alt="change" />
-                    <div className={styles.gauge}>
-                        <div className={styles.fill} style={{ width: '50%' }}></div>
-                    </div>
-                </div>
-                <div className={styles.face}>
-                    <img src={'/images/face4.png'} alt="change" />
-                    <div className={styles.gauge}>
-                        <div className={styles.fill} style={{ width: '50%' }}></div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     );
